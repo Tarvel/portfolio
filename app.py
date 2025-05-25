31c94880e7a94403206598de3784f4c5f31a4853
tarvel <taiyebabatundejames@gmail.com>
2025-05-25 09:12:57 +0100

    update

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file
 
 app = Flask(__name__)
 
@@ -12,6 +12,12 @@ def index():
 def projects():
     
     return render_template('projects.html')
+
+@app.route('/download')
+def download_file():
+    path = "static/Taiye_s_Resume.pdf"
+    return send_file(path, as_attachment=True, download_name="Taiye_s_Resume.pdf")
+
 
 
 if __name__ == '__main__':
